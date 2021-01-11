62f3a59e3057b50d9a71dd0539c753ca9fd36cbf
Kleiner <megaadrenaline1055@gmail.com>
2021-01-12 00:29:34 +0200

    checkboxes
    This is bs. Dont do this at home. Pls.

---
+++ checkbox_api.js
@@ -0,0 +1,96 @@
+// toggle drowpodwn
+
+$(document).ready(function(){
+  $("#dropdown1").click(function(){
+    $("#dropdownmenu1").toggleClass("hidden_display_none");
+    
+ 
+  });
+});
+
+
+
+
+// toggle drowpodwn 2
+
+$(document).ready(function(){
+  $("#dropdown2").click(function(){
+    $("#dropdownmenu2").toggleClass("hidden_display_none");
+    
+ 
+  });
+});
+
+
+
+
+
+// square checkboxes
+$(document).ready(function(){
+  $(".mrk_checkbox").click(function(){
+    
+    if(this.classList.contains('cbox_blocked'))
+    {
+      
+    }
+    else
+     {
+        $(this).find(".mrk_checkbox_tickmark").toggleClass("checkbox_inner_unset");
+        $(this).find(".mrk_checkbox_tickmark").toggleClass("checkbox_set_true");
+     }
+  });
+});
+
+
+
+// mrk_checkbox_group
+
+
+// round checkboxes
+$(document).ready(function(){
+  $(".mrk_checkbox_override").click(function(){
+    
+    if(this.classList.contains('cbox_blocked'))
+    {
+      
+    }
+    else
+     {
+        $(this).find(".mrk_checkbox_tickmark").toggleClass("checkbox_inner_unset");
+        $(this).find(".mrk_checkbox_tickmark").toggleClass("checkbox_set_true");
+        
+          
+        var current_container = this.closest(".mrk_checkbox_group");
+
+        var current_row_row = $(this).attr('mrk_crow');
+       
+       
+       // block all square checkboxes 
+       $(current_container).find(".mrk_checkbox").toggleClass("cbox_blocked");
+       
+       // block all round override checkboxes
+       $(current_container).find(".mrk_checkbox_override").toggleClass("cbox_blocked");
+       
+       // block override all
+       $(current_container).find(".checkbox_inner_sqare").toggleClass("cbox_set_false_override");
+
+       
+       // unblock current row round checkbox
+     
+       $(current_container).find("[mrk_crow=\"" + current_row_row + "\"]").removeClass("cbox_set_false_override");
+       
+       $(current_container).find("[mrk_crow=\"" + current_row_row + "\"]").removeClass("cbox_blocked");
+       
+       
+       
+       // remove death tag
+       $(current_container).find("[mrk_crow=\"" + current_row_row + "\"]").removeClass("checkbox_inner_unset");
+       // now make set state to true
+       $(current_container).find("[mrk_crow=\"" + current_row_row + "\"]").addClass("checkbox_set_true");
+       
+       
+
+ 
+     }
+  });
+});
--- vmat_reader_noauto.js
+++ vmat_reader_noauto.js
@@ -115,10 +115,56 @@ var wannadie;
                               }  
 
                               // alert (theme_bg);
+							  var global_dropdown_id = document.querySelector("#dropdown_vmat_file");		
+
+
+
+
+								var theme_bg_cbox = $(global_dropdown_id).find("[mrk_crow=\"mrow16\"]");
+							  
+							  var btex_color_cbox = $(global_dropdown_id).find("[mrk_crow=\"mrow1\"]");
+							  
+							  var prox_names = $(global_dropdown_id).find("[mrk_crow=\"mrow2\"]");
+							  
+							  var prox_params = $(global_dropdown_id).find("[mrk_crow=\"mrow3\"]");
+							  
+							  var dola_params = $(global_dropdown_id).find("[mrk_crow=\"mrow5\"]");
+							  
+							  var per_params = $(global_dropdown_id).find("[mrk_crow=\"mrow4\"]");
+							  
+							  var num_cbox = $(global_dropdown_id).find("[mrk_crow=\"mrow6\"]");
+							  
+							  var cvars_cbox = $(global_dropdown_id).find("[mrk_crow=\"mrow7\"]");
+							  
+							  var s_fg_cbox = $(global_dropdown_id).find("[mrk_crow=\"mrow8\"]");
+							  
+							  var s_bg_cbox = $(global_dropdown_id).find("[mrk_crow=\"mrow9\"]");
+							  
+							  var w_proxy_cbox = $(global_dropdown_id).find("[mrk_crow=\"mrow11\"]");
+							  
+							  var mtypes_cbox = $(global_dropdown_id).find("[mrk_crow=\"mrow12\"]");
+							  
+							  var comms_cbox = $(global_dropdown_id).find("[mrk_crow=\"mrow13\"]");
+							  
+							  var dsign_psign_cbox = $(global_dropdown_id).find("[mrk_crow=\"mrow14\"]");
+							  
+							  var sq_bracks_cbox = $(global_dropdown_id).find("[mrk_crow=\"mrow15\"]");
+							  
+							  var def_bracks_cbox = $(global_dropdown_id).find("[mrk_crow=\"mrow17\"]");							  
 							  
 							  
 							  
 							  
+							  
+							  if(theme_bg_cbox[1].classList.contains('cbox_set_false_override'))
+							  {
+								  console.log("no_override");
+							  }
+							  else
+							  {
+								  console.log("is_it_set");
+								if(theme_bg_cbox[1].classList.contains('checkbox_set_true'))
+							  {							  
 							  
 							  
 							  // if (theme_bg_load != test_comp)
@@ -130,6 +176,7 @@ var wannadie;
 								document.getElementById("cd_theme_bg_picker").value = theme_bg_load;
 								$(".line").css('background', "#" +  theme_bg_load);
 							  // }
+							  }}
 
 
 
@@ -141,7 +188,15 @@ var wannadie;
 
 
 
-
+							  if(btex_color_cbox[1].classList.contains('cbox_set_false_override'))
+							  {
+								  console.log("no_override");
+							  }
+							  else
+							  {
+								  console.log("is_it_set");
+								if(btex_color_cbox[1].classList.contains('checkbox_set_true'))
+							  {
 							  // if (btex_col_load != btex_col_defcomp)
 							  // {
 								// btex_col
@@ -153,164 +208,420 @@ var wannadie;
 								// v feedb
 								$(".base_texture").css('color', "#" +  btex_col_load);							
 							  // }
-                            
-	// if (proxy_names_load != proxy_names_defcomp)
-	// {							
-	// proxy names						
-	document.getElementById("pr_names_picker").value = "#" + proxy_names_load;
-	// text input
-	document.getElementById("cd_pr_names_picker").value = proxy_names_load;
-	// vis feedb
-	$(".proxy_names").css('color', "#" +  proxy_names_load); 
-	// }
+							  }}
+							  
+							  
+							  
+							  
+							  
+							  
+							  
+							  
+							  if(prox_names[1].classList.contains('cbox_set_false_override'))
+							  {
+								  // console.log("no_override");
+							  }
+							  else
+							  {
+								  // console.log("is_it_set");
+								if(prox_names[1].classList.contains('checkbox_set_true'))
+							  {	                            
+								// if (proxy_names_load != proxy_names_defcomp)
+								// {							
+								// proxy names						
+								document.getElementById("pr_names_picker").value = "#" + proxy_names_load;
+								// text input
+								document.getElementById("cd_pr_names_picker").value = proxy_names_load;
+								// vis feedb
+								$(".proxy_names").css('color', "#" +  proxy_names_load); 
+								// }
+								}}
+								
+								
+								
+								
+								
+								
+								
+								
 
-	// if (proxy_parameters_load != proxy_parameters_defcomp)
-	// {	
-	// proxy params	
-	document.getElementById("pr_params_picker").value = "#" + proxy_parameters_load;
-	// text input
-	document.getElementById("cd_pr_params_picker").value = proxy_parameters_load;
-	// vis feedb
-	$(".proxy_params").css('color', "#" +  proxy_parameters_load);	
-	// }
+							  if(prox_params[1].classList.contains('cbox_set_false_override'))
+							  {
+								  // console.log("no_override");
+							  }
+							  else
+							  {
+								  // console.log("is_it_set");
+								if(prox_params[1].classList.contains('checkbox_set_true'))
+							  {								
+
+								// if (proxy_parameters_load != proxy_parameters_defcomp)
+								// {	
+								// proxy params	
+								document.getElementById("pr_params_picker").value = "#" + proxy_parameters_load;
+								// text input
+								document.getElementById("cd_pr_params_picker").value = proxy_parameters_load;
+								// vis feedb
+								$(".proxy_params").css('color', "#" +  proxy_parameters_load);	
+								// }
+								}}
+								
+								
+								
+								
+								
+								
+								
+								
+							  if(dola_params[1].classList.contains('cbox_set_false_override'))
+							  {
+								  // console.log("no_override");
+							  }
+							  else
+							  {
+								  // console.log("is_it_set");
+								if(dola_params[1].classList.contains('checkbox_set_true'))
+							  {									
+								
+								// if (dol_parameters_load != dol_parameters_defcomp)
+								// {	
+								// dollar params	
+								document.getElementById("dol_params_picker").value = "#" + dol_parameters_load;
+								// text input
+								document.getElementById("set_def_dol_params").value = dol_parameters_load;
+								// vis feedb
+								$(".dol_params").css('color', "#" +  dol_parameters_load);
+								// }
+								}}
+								
+								
+								
+								
+								
+								
+								
+								
+							  if(per_params[1].classList.contains('cbox_set_false_override'))
+							  {
+								  // console.log("no_override");
+							  }
+							  else
+							  {
+								  // console.log("is_it_set");
+								if(per_params[1].classList.contains('checkbox_set_true'))
+							  {									
+								
+								
+								
+								// if (perc_parameters_load != perc_parameters_defcomp)
+								// {
+								// perc params
+								document.getElementById("perc_params_picker").value = "#" + perc_parameters_load;
+								// text input
+								document.getElementById("cd_perc_params_picker").value = perc_parameters_load;
+								// vis feedb
+								$(".perc_params_aw").css('color', "#" +  perc_parameters_load);
+								// }
+								}}
+								
+								
+								
+								
+								
+								
+								
+								
+								
+								
+								
+								
+							  if(num_cbox[1].classList.contains('cbox_set_false_override'))
+							  {
+								  // console.log("no_override");
+							  }
+							  else
+							  {
+								  // console.log("is_it_set");
+								if(num_cbox[1].classList.contains('checkbox_set_true'))
+							  {									
+								
+								// if (numbers_load != numbers_defcomp)
+								// {	
+								// numbers
+								document.getElementById("num_col_picker").value = "#" + numbers_load;
+								// text input
+								document.getElementById("cd_nums_picker").value = numbers_load;
+								// vis feedb
+								$(".p_nums").css('color', "#" +  numbers_load);
+								// }
+								}}
+								
+								
+								
+								
+								
+								
+								
+								
+								
+								
+							  if(cvars_cbox[1].classList.contains('cbox_set_false_override'))
+							  {
+								  // console.log("no_override");
+							  }
+							  else
+							  {
+								  // console.log("is_it_set");
+								if(cvars_cbox[1].classList.contains('checkbox_set_true'))
+							  {									
+								
+								
+								// if (cvars_def_load != cvars_def_defcomp)
+								// {		
+								// cvars w def
+								document.getElementById("cvars_c_picker").value = "#" + cvars_def_load;
+								// text input
+								document.getElementById("cd_cvars_w_def_picker").value = cvars_def_load;
+								// vis feedb
+								$(".noname_cvars").css('color', "#" +  cvars_def_load);
+								$(".t_path").css('color', "#" +  cvars_def_load);
+								$(".qmark").css('color', "#" +  cvars_def_load);
+								// }
+								}}
+								
+								
+								
+								
+								
+								
+								
+							  if(s_fg_cbox[1].classList.contains('cbox_set_false_override'))
+							  {
+								  // console.log("no_override");
+							  }
+							  else
+							  {
+								  // console.log("is_it_set");
+								if(s_fg_cbox[1].classList.contains('checkbox_set_true'))
+							  {								
+								
+								
+								// if (shader_fg_load != shader_fg_defcomp)
+								// {		
+								// shader fg
+								document.getElementById("shader_fg_picker").value = "#" + shader_fg_load;
+								// text input
+								document.getElementById("cd_shader_picker").value = shader_fg_load;
+								// vis feedb
+								$(".shaders").css('color', "#" +  shader_fg_load);
+								// }
+								}}
+								
+								
+								
+								
+								
+								
+								
+								
+								
+							  if(s_bg_cbox[1].classList.contains('cbox_set_false_override'))
+							  {
+								  // console.log("no_override");
+							  }
+							  else
+							  {
+								  // console.log("is_it_set");
+								if(s_bg_cbox[1].classList.contains('checkbox_set_true'))
+							  {									
+								
+								
+								// if (shader_bg_load != shader_bg_defcomp)
+								// {	
+								// shader bg
+								document.getElementById("shader_bg_picker").value = "#" + shader_bg_load;
+								// text input
+								document.getElementById("cd_shader_bg_picker").value = shader_bg_load;
+								// vis feedb
+								$(".shaders").css('background', "#" +  shader_bg_load);
+								// }
+								}}
+								
+								
+								
+								
+								
+								
+							  if(w_proxy_cbox[1].classList.contains('cbox_set_false_override'))
+							  {
+								  // console.log("no_override");
+							  }
+							  else
+							  {
+								  // console.log("is_it_set");
+								if(w_proxy_cbox[1].classList.contains('checkbox_set_true'))
+							  {										
+								
+								
+								// if (wproxy_load != wproxy_defcomp)
+								// {		
+								// w proxy
+								document.getElementById("word_proxy_c_picker").value = "#" + wproxy_load;
+								// text input
+								document.getElementById("cd_wproxy_bg_picker").value = wproxy_load;
+								// vis feedb
+								$(".word_proxy").css('color', "#" +  wproxy_load);
+								// }
+								}}
+								
+								
+								
+								
+								
+								
+								
+							  if(mtypes_cbox[1].classList.contains('cbox_set_false_override'))
+							  {
+								  // console.log("no_override");
+							  }
+							  else
+							  {
+								  // console.log("is_it_set");
+								if(mtypes_cbox[1].classList.contains('checkbox_set_true'))
+							  {									
+								
+								
+								// if (mat_types_load != mat_types_defcomp)
+								// {		
+								// mat types
+								document.getElementById("mat_types_c_picker").value = "#" + mat_types_load;
+								// text input
+								document.getElementById("cd_mtypes_bg_picker").value = mat_types_load;
+								// vis feedb
+								$(".mat_types").css('color', "#" +  mat_types_load);	
+								// }
+								}}
+								
+								
+								
+								
+								
+								
+							  if(dsign_psign_cbox[1].classList.contains('cbox_set_false_override'))
+							  {
+								  // console.log("no_override");
+							  }
+							  else
+							  {
+								  // console.log("is_it_set");
+								if(dsign_psign_cbox[1].classList.contains('checkbox_set_true'))
+							  {									
+								
+								
+								
+								// if (dsign_psign_load != dsign_psign_defcomp)
+								// {	
+								// dolsign w psign
+								document.getElementById("dsign_c_picker").value = "#" + dsign_psign_load;
+								// text input
+								document.getElementById("cd_dpsign_bg_picker").value = dsign_psign_load;
+								// vis feedb
+								$(".dolsign").css('color', "#" + dsign_psign_load);	
+								// }
+								}}
+								
+								
+								
+								
+								
+								
+								
+								
+							  if(sq_bracks_cbox[1].classList.contains('cbox_set_false_override'))
+							  {
+								  // console.log("no_override");
+							  }
+							  else
+							  {
+								  // console.log("is_it_set");
+								if(sq_bracks_cbox[1].classList.contains('checkbox_set_true'))
+							  {								
+								
+								
+								// if (sq_bracks_load != sq_bracks_defcomp)
+								// {		
+								// sq bracks
+								document.getElementById("sq_bracks_c_picker").value = "#" + sq_bracks_load;
+								// text input
+								document.getElementById("cd_sqb_bg_picker").value = sq_bracks_load;
+								// vis feedb
+								$(".sq_brackets_open").css('color', "#" +  sq_bracks_load);
+								
+								$(".sq_brackets_close").css('color', "#" +  sq_bracks_load);
+								// }
+								}}
+								
+								
+								
+								
+								
+								
+								
+								
+								
+							  if(def_bracks_cbox[1].classList.contains('cbox_set_false_override'))
+							  {
+								  // console.log("no_override");
+							  }
+							  else
+							  {
+								  // console.log("is_it_set");
+								if(def_bracks_cbox[1].classList.contains('checkbox_set_true'))
+							  {									
+								
+								
+								// if (def_bracks_load != def_bracks_defcomp)
+								// {		
+								// Def bracks
+								document.getElementById("def_bracks_c_picker").value = "#" + def_bracks_load;
+								// text input
+								document.getElementById("cd_defb_bg_picker").value = def_bracks_load;
+								// vis feedb
+								$(".def_brackets_open").css('color', "#" +  def_bracks_load);
+								
+								$(".def_brackets_close").css('color', "#" +  def_bracks_load);
+								// }
+								}}
 	
-	// if (dol_parameters_load != dol_parameters_defcomp)
-	// {	
-	// dollar params	
-	document.getElementById("dol_params_picker").value = "#" + dol_parameters_load;
-	// text input
-	document.getElementById("set_def_dol_params").value = dol_parameters_load;
-	// vis feedb
-	$(".dol_params").css('color', "#" +  dol_parameters_load);
-	// }
-	
-	
-	// if (perc_parameters_load != perc_parameters_defcomp)
-	// {
-	// perc params
-	document.getElementById("perc_params_picker").value = "#" + perc_parameters_load;
-	// text input
-	document.getElementById("cd_perc_params_picker").value = perc_parameters_load;
-	// vis feedb
-	$(".perc_params_aw").css('color', "#" +  perc_parameters_load);
-	// }
-	
-	
-	// if (numbers_load != numbers_defcomp)
-	// {	
-	// numbers
-	document.getElementById("num_col_picker").value = "#" + numbers_load;
-	// text input
-	document.getElementById("cd_nums_picker").value = numbers_load;
-	// vis feedb
-	$(".p_nums").css('color', "#" +  numbers_load);
-	// }
-	
-	
-	
-	// if (cvars_def_load != cvars_def_defcomp)
-	// {		
-	// cvars w def
-	document.getElementById("cvars_c_picker").value = "#" + cvars_def_load;
-	// text input
-	document.getElementById("cd_cvars_w_def_picker").value = cvars_def_load;
-	// vis feedb
-	$(".noname_cvars").css('color', "#" +  cvars_def_load);
-	$(".t_path").css('color', "#" +  cvars_def_load);
-	$(".qmark").css('color', "#" +  cvars_def_load);
-	// }
-	
-	// if (shader_fg_load != shader_fg_defcomp)
-	// {		
-	// shader fg
-	document.getElementById("shader_fg_picker").value = "#" + shader_fg_load;
-	// text input
-	document.getElementById("cd_shader_picker").value = shader_fg_load;
-	// vis feedb
-	$(".shaders").css('color', "#" +  shader_fg_load);
-	// }
-	
-	
-	
-	// if (shader_bg_load != shader_bg_defcomp)
-	// {	
-	// shader bg
-	document.getElementById("shader_bg_picker").value = "#" + shader_bg_load;
-	// text input
-	document.getElementById("cd_shader_bg_picker").value = shader_bg_load;
-	// vis feedb
-	$(".shaders").css('background', "#" +  shader_bg_load);
-	// }
-	
-	// if (wproxy_load != wproxy_defcomp)
-	// {		
-	// w proxy
-	document.getElementById("word_proxy_c_picker").value = "#" + wproxy_load;
-	// text input
-	document.getElementById("cd_wproxy_bg_picker").value = wproxy_load;
-	// vis feedb
-	$(".word_proxy").css('color', "#" +  wproxy_load);
-	// }
-	
-	// if (mat_types_load != mat_types_defcomp)
-	// {		
-	// mat types
-	document.getElementById("mat_types_c_picker").value = "#" + mat_types_load;
-	// text input
-	document.getElementById("cd_mtypes_bg_picker").value = mat_types_load;
-	// vis feedb
-	$(".mat_types").css('color', "#" +  mat_types_load);	
-	// }
-	
-	// if (dsign_psign_load != dsign_psign_defcomp)
-	// {	
-	// dolsign w psign
-	document.getElementById("dsign_c_picker").value = "#" + dsign_psign_load;
-	// text input
-	document.getElementById("cd_dpsign_bg_picker").value = dsign_psign_load;
-	// vis feedb
-	$(".dolsign").css('color', "#" + dsign_psign_load);	
-	// }
-	
-	
-	// if (sq_bracks_load != sq_bracks_defcomp)
-	// {		
-	// sq bracks
-	document.getElementById("sq_bracks_c_picker").value = "#" + sq_bracks_load;
-	// text input
-	document.getElementById("cd_sqb_bg_picker").value = sq_bracks_load;
-	// vis feedb
-	$(".sq_brackets_open").css('color', "#" +  sq_bracks_load);
-	
-	$(".sq_brackets_close").css('color', "#" +  sq_bracks_load);
-	// }
 	
 	
 	
 	
 	
-	// if (def_bracks_load != def_bracks_defcomp)
-	// {		
-	// Def bracks
-	document.getElementById("def_bracks_c_picker").value = "#" + def_bracks_load;
-	// text input
-	document.getElementById("cd_defb_bg_picker").value = def_bracks_load;
-	// vis feedb
-	$(".def_brackets_open").css('color', "#" +  def_bracks_load);
-	
-	$(".def_brackets_close").css('color', "#" +  def_bracks_load);
-	// }
 	
 	
-	//Comments
 	
+							  if(comms_cbox[1].classList.contains('cbox_set_false_override'))
+							  {
+								  // console.log("no_override");
+							  }
+							  else
+							  {
+								  // console.log("is_it_set");
+								if(comms_cbox[1].classList.contains('checkbox_set_true'))
+							  {		
 	
-	// color picker
-	document.getElementById("comms_c_picker").value = "#" + comments_load;
-	// text input
-	document.getElementById("cd_comms_bg_picker").value = comments_load;
-	// vis feedb
-	$(".comms").css('color', "#" +  comments_load);
+								//Comments
+								
+								
+								// color picker
+								document.getElementById("comms_c_picker").value = "#" + comments_load;
+								// text input
+								document.getElementById("cd_comms_bg_picker").value = comments_load;
+								// vis feedb
+								$(".comms").css('color', "#" +  comments_load);
+								
+								}}
                             
                            
                             
