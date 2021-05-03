d5856ff3f1b6efe776bae5cc3dbbf37c10cfab62
Mr.Kleiner <megaadrenaline1055@gmail.com>
2021-05-03 18:54:44 +0300

    more fixes 17

---
+++ shorties.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $(".dl_old_show").click(function(){
+		
+		
+	$(this).closest("div").addClass("hidden_display_none");
+		
+	// $(this).siblings(".mrk_checkbox_group_is_dropdown").toggleClass("hidden_display_none");
+    // $(".dl_old_show").addClass("hidden_display_none");
+	
+	$(".dl_old_hidden").removeClass("hidden_display_none");
+	
+	
+	// manage other dropdowns
+	
+	
+ 
+  });
+});
